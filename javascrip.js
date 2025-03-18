927793d33c284b1c05452097cbf868b810f14a63
alumno <alumno@Instituto.local>
2025-03-18 09:50:48 -0300

    Update javascrip.js

--- javascrip.js
+++ javascrip.js
@@ -1,4 +1,13 @@
 let añoNacimiento = 2008
 let añoActual = 2025
 
-alert (("Mi edad es" )+( añoActual-añoNacimiento))
+alert (("Mi edad es " )+( añoActual-añoNacimiento))
+let nombrealumno = "jorge"
+let aprobado = true
+let edad = 17
+if (edad>=17) {
+  alert ("podes ingresar al boliche")  
+}
+else{
+    alert("no podes ingresar")
+}
