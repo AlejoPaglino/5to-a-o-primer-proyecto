73ddb983970aa865d57448cceac249d09810b6f8
alumno <alumno@Instituto.local>
2025-03-19 08:21:29 -0300

    Update javascrip.js

--- javascrip.js
+++ javascrip.js
@@ -11,3 +11,14 @@ if (edad>=17) {
 else{
     alert("no podes ingresar")
 }
+
+let AsistenciaAFutbol= prompt ("¿cuantos dias viniste esta semana?")
+let ParteFisica= prompt ("¿diste el 100% en la parte fisica?")
+let RendimientoEnElPartido = prompt("¿como jugo el finde pasado?")
+if ((AsistenciaAFutbol==5) && (ParteFisica=("si")) && (RendimientoEnElPartido>=7))
+{
+  alert ("mas chances de estar citado")
+} 
+else {
+ alert ("tenes que mejorar para poder estar citado")
+}
